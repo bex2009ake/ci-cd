bacc028d081512d8f8b28feb164b773dfccd0945
bex2009ake <moneybusinessdollar@gmail.com>
2024-07-19 19:49:31 +0500

    cicd

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def resp(res):
-    return HttpResponse('<h1>Hello, CiCd</h1>  <br > <h2>How are you ???</h2> <br > <h3>Do you like Devops ???</h3>')
+    return HttpResponse('<h1>Hello, Devops</h1')
